a441552248b94e7a84adb3959769f299cea06f95
server0707 <abdullajon_uzb@mail.ru>
2021-09-18 16:05:04 +0500

    Loyiha fayllari yuklandi

---
+++ SnakeCsharp/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SnakeCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::SnakeCsharp.Properties.Resources._3;
+            this.pictureBox4.Location = new System.Drawing.Point(219, 182);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::SnakeCsharp.Properties.Resources._2;
+            this.pictureBox3.Location = new System.Drawing.Point(275, 182);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SnakeCsharp.Properties.Resources._1;
+            this.pictureBox2.Location = new System.Drawing.Point(331, 182);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SnakeCsharp.Properties.Resources.Pirate;
+            this.pictureBox1.Location = new System.Drawing.Point(387, 182);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(536, 434);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+
+    }
+}
+
---
+++ SnakeCsharp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeCsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = pictureBox1.Location.X, y = pictureBox1.Location.Y,x1,y1;
+            //tepaga
+            if (e.KeyCode.ToString() == "W" && pictureBox1.Location.Y-55 > 0)
+            {
+                if ((pictureBox2.Location.X != x && pictureBox2.Location.Y != y - 55) ||
+                   (pictureBox3.Location.X != x && pictureBox3.Location.Y != y - 55) ||
+                   (pictureBox4.Location.X != x && pictureBox4.Location.Y != y - 55))
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 55);
+                    x1 = pictureBox2.Location.X;
+                    y1 = pictureBox2.Location.Y;
+                    pictureBox2.Location = new Point(x, y);
+                    x = pictureBox3.Location.X;
+                    y = pictureBox3.Location.Y;
+                    pictureBox3.Location = new Point(x1, y1);
+                    pictureBox4.Location = new Point(x, y);
+                }
+            }
+            //pastga
+            if (e.KeyCode.ToString() == "S" && pictureBox1.Location.Y < this.Height - 150) 
+            {
+                if ((pictureBox2.Location.X != x && pictureBox2.Location.Y != y + 55) ||
+                   (pictureBox3.Location.X != x && pictureBox3.Location.Y != y + 55) ||
+                   (pictureBox4.Location.X != x && pictureBox4.Location.Y != y + 55))
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 55);
+                    x1 = pictureBox2.Location.X;
+                    y1 = pictureBox2.Location.Y;
+                    pictureBox2.Location = new Point(x, y);
+                    x = pictureBox3.Location.X;
+                    y = pictureBox3.Location.Y;
+                    pictureBox3.Location = new Point(x1, y1);
+                    pictureBox4.Location = new Point(x, y);
+                }
+            }
+            //chapga
+            if (e.KeyCode.ToString() == "A" && pictureBox1.Location.X-55 > 0)
+            {
+                if ((pictureBox2.Location.X != x - 55 && pictureBox2.Location.Y != y) ||
+                   (pictureBox3.Location.X != x - 55 && pictureBox3.Location.Y != y) ||
+                   (pictureBox4.Location.X != x - 55 && pictureBox4.Location.Y != y))
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - 55, pictureBox1.Location.Y);
+                    x1 = pictureBox2.Location.X;
+                    y1 = pictureBox2.Location.Y;
+                    pictureBox2.Location = new Point(x, y);
+                    x = pictureBox3.Location.X;
+                    y = pictureBox3.Location.Y;
+                    pictureBox3.Location = new Point(x1, y1);
+                    pictureBox4.Location = new Point(x, y);
+                }
+            }
+            //ongga
+            if (e.KeyCode.ToString() == "D" && pictureBox1.Location.X+55 < this.Width-55)
+            {
+                if ((pictureBox2.Location.X != x + 55 && pictureBox2.Location.Y != y) ||
+                 (pictureBox3.Location.X != x + 55 && pictureBox3.Location.Y != y) ||
+                 (pictureBox4.Location.X != (x + 55) && pictureBox4.Location.Y != y))
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 55, pictureBox1.Location.Y);
+                    x1 = pictureBox2.Location.X;
+                    y1 = pictureBox2.Location.Y;
+                    pictureBox2.Location = new Point(x, y);
+                    x = pictureBox3.Location.X;
+                    y = pictureBox3.Location.Y;
+                    pictureBox3.Location = new Point(x1, y1);
+                    pictureBox4.Location = new Point(x, y);
+                }
+            }
+        }
+    }
+}
